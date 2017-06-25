1d3bb5f695ae829dda098578021de95a4d525beb
pboserman <pabloboserman@gmail.com>
2017-06-25 16:19:39 -0700

    (feat) implements basic working facebook login feature without logout

--- server/app.js
+++ server/app.js
@@ -2,14 +2,33 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var util = require('./middleware');
-
+var passport = require('passport');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, '/../client/public')));
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-app.get('/login', (req, res) => {
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/login', express.static(path.join(__dirname, '/../client/public/login')));
+
+app.get('/login/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/return', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log('Authenticated');
+    res.redirect('/');
+  });
+
+app.use('/',
+  require('connect-ensure-login').ensureLoggedIn(),
+  express.static(path.join(__dirname, '/../client/public/index')));
 
 
 module.exports = app;
