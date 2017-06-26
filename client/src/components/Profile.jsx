fbe4b70be0642cef51aef9b02bdb86d91b1a536d
pboserman <pabloboserman@gmail.com>
2017-06-26 16:49:25 -0700

    (refactor) adds btn class to buttons

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -10,21 +10,21 @@ const Profile = () => (
     <p>Frank@test.com</p>
     <label>New Email</label>
     <input type="text"/>
-    <button>Change Email</button>
+    <button className="btn">Change Email</button>
     <br></br>
     <br></br>
     <p>Phone Number: </p>
     <p>(415)555-5555</p>
     <label>New Phone Number</label>
     <input type="text"/>
-    <button>Change Phone Number</button>
+    <button className="btn">Change Phone Number</button>
     <br></br>
     <br></br>
     <p>Location: </p>
     <p>SOMA, San Francisco</p>
     <label>New Location</label>
     <input type="text"/>
-    <button>Change Location</button>
+    <button className="btn">Change Location</button>
   </div>
 );
 
