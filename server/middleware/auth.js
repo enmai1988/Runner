802f5198f55b978ed0946d20f0c5f96b7d89a4c4
Kai Mashima <kaiomashima@gmail.com>
2017-06-25 18:17:47 -0700

    (bug) adds changes to auth and package.json to allow for local running

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,12 +2,13 @@ var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
 
 // NEED TO GET APP KEY FOR THIS FILE
-// var config = require('../config/config.js');
+var config = require('../config/config.js');
 
 passport.use(new Strategy({
   clientID: process.env.RUNNER_ID || config.FB_ID,
   clientSecret: process.env.RUNNER_SECRET || config.FB_SECRET,
-  callbackURL: 'https://effective-elephants-runner.herokuapp.com/login/facebook/return'
+  callbackURL: '/login/facebook/return'
+  //callbackURL: 'https://effective-elephants-runner.herokuapp.com/login/facebook/return'
 },
 function(accessToken, refreshToken, profile, cb) {
   return cb(null, profile);
