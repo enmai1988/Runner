97ec8ff03f00546ef33aa7f24b1f990c2195133e
Kai Mashima <kaiomashima@gmail.com>
2017-06-28 11:58:09 -0700

    fixing bugs

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import HomeRunEntry from './HomeRunEntry.jsx';
 
 const Home = (props) => (
   <div className="reactComponentContainer">
     <h2>Home</h2>
     <div>
       <ul className="homeRunsContainer">
-        {props.runs.map((run, index) => {
-          return <HomeRunEntry run={run} acceptRun={props.acceptRun} key={index} />
-        })}
+        <li className="homeRunsItem homeRunContainer">
+          <div className="homeRun">
+            <h4>Run Title</h4><p>Run</p>
+          </div>
+          <div className="homeRun">
+            <h4>Time</h4><p>6:00pm</p>
+          </div>
+          <div className="homeRun"> 
+            <h4>Payout</h4><p>$5</p> 
+          </div>
+          <div className="homeRun">
+            <button className="btn" onClick={props.acceptRun}>Accept Run</button>
+          </div>
+        </li>
+        <li className="homeRunsItem homeRunContainer">
+          <div className="homeRun">
+            <h4>Run Title</h4><p>Run</p>
+          </div>
+          <div className="homeRun">
+            <h4>Time</h4><p>6:00pm</p>
+          </div>
+          <div className="homeRun"> 
+            <h4>Payout</h4><p>$5</p> 
+          </div>
+          <div className="homeRun">
+            <button className="btn" onClick={props.acceptRun}>Accept Run</button>
+          </div>
+        </li>
       </ul>
     </div>
   </div>
@@ -16,4 +40,11 @@ const Home = (props) => (
 
 export default Home;
 
-
+// {props.runs.map((run, index) => {
+//   return  <li className="homeRunItem">
+//             <h4>Run Title</h4><p>{runs.title}</p>
+//             <h4>Time</h4><p>{run.time}</p> 
+//             <h4>Payout</h4><p>{run.payout}</p> 
+//             <button className="btn" onClick={props.acceptRun}>Accept Run</button>
+//           </li>
+// })}
