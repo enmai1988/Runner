ec509a7ae72a67fed31bc66b96d4bdd9ef76e4f5
Jeff Shindelus <jeff.shindelus@gmail.com>
2017-06-30 18:30:46 -0700

    added more db functions, tested all db functions, and added fb user data

--- database/models/run.js
+++ database/models/run.js
@@ -1,5 +1,10 @@
 const Model = require('./model');
 const Users = require('./user');
+const db = require('../index.js');
+var pg = require('pg');
+const Promise = require('bluebird');
+
+console.log('in models/run.js')
 
 class Runs extends Model {
   constructor() {
@@ -7,27 +12,27 @@ class Runs extends Model {
   }
 
   create(runObj) {
-    return db.query(`INSERT INTO RUNS (userid, amount, location, status, title, description) values (${runObj.userid}, ${runObj.amount}, ${runObj.location}, ${runObj.status}, ${runObj.title}, ${runObj.description})`);
+    return db.query(`INSERT INTO RUNS (userId, amount, location, status, title, description) values (${runObj.userId}, '${runObj.amount}', '${runObj.location}', '${runObj.status}', '${runObj.title}', '${runObj.description}')`);
   }
 
-  getAllAvailableRuns() {
-    return db.query(`SELECT amount, location, status, title, description FROM RUNS WHERE status = 'available`);
-  }
-
-  getAllFinishedRuns() {
-    return db.query(`SELECT amount, location, status, title, description FROM RUNS WHERE status = 'finished`);
+  getAllRunsWithStatus(status) {
+    return db.query(`SELECT amount, location, status, title, description FROM RUNS WHERE status = '${status}'`);
   }
 
   deleteRun(runObj) {
-    return db.query(`DELETE FROM RUNS WHERE DESCRIPTION = ${runObj.description}`)
+    return db.query(`DELETE FROM RUNS WHERE DESCRIPTION = '${runObj.description}'`)
   }
 
-  addRunner(userObj) {
-    return db.query(`INSERT INTO RUNS (runnerid) values (SELECT ID FROM USERS WHERE EMAIL = ${userObj.email})`);
+  addRunner(runObj, userId) {
+    return db.query(`UPDATE RUNS SET runnerid = ${userId} WHERE description = '${runObj.description}'`);
   }
 
-  finishRun(runObj) {
-    return db.query("UPDATE RUNS SET STATUS = 'finished'")
+  updateStatus(runObj, status) {
+    return db.query(`UPDATE RUNS SET STATUS = '${status}' WHERE description = '${runObj.description}'`);
+  }
+
+  updateRun(runObj) {
+    return db.query(`UPDATE RUNS SET userId = ${runObj.userId}, location = '${runObj.location}', status = '${runObj.status}', title = '${runObj.title}', description = '${runObj.description}' WHERE title = '${runObj.title}'`);
   }
 
 }
