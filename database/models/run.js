9dee01797676d017a68af0c3d9f67e985c5a8aa7
Jeff Shindelus <jeff.shindelus@gmail.com>
2017-07-01 19:47:08 -0700

    fix more merge conflicts

--- database/models/run.js
+++ database/models/run.js
@@ -12,7 +12,11 @@ class Runs extends Model {
   }
 
   create(runObj) {
-    return db.query(`INSERT INTO RUNS (userId, amount, location, status, title, description, ) values (${runObj.userId}, '${runObj.amount}', '${runObj.location}', '${runObj.status}', '${runObj.title}', '${runObj.description}')`);
+    return db.query(`INSERT INTO RUNS (userId, amount, location, status, title, description, expectedFinishTime) values (${runObj.userId}, '${runObj.amount}', '${runObj.location}', '${runObj.status}', '${runObj.title}', '${runObj.description}', '${runObj.expectedFinishTime}')`);
+  }
+
+  getAllRunsWithUserId(userId) {
+    return db.query(`SELECT amount, location, status, title, description FROM RUNS WHERE userId = '${userId}'`);
   }
 
   getAllRunsWithStatus(status) {
@@ -27,21 +31,18 @@ class Runs extends Model {
     return db.query(`UPDATE RUNS SET runnerid = ${userId} WHERE description = '${runObj.description}'`);
   }
 
-  updateStatus(runObj, status) {
-    // Can we just get the status from the run object instead of passing it as a parameter here?
+  updateStatus(runObj) {
     let date = new Date();
     let dateStamp = date.toLocaleString();
-    if (status === 'finished') {
-      return db.query(`UPDATE RUNS SET STATUS = '${status}', finishTime = '${dateStamp}' WHERE id = '${runObj.id}'`);
-    } else if (status === 'started') {
-      // shouldn't the status be changed to finished here? instead of setting it to started again?
-      // also maybe we should add a third if statement for available -> started
-      return db.query(`UPDATE RUNS SET STATUS = '${status}', startTime = '${dateStamp}' WHERE id = '${runObj.id}'`);
+    if (runObj.status === 'available') {
+      return db.query(`UPDATE RUNS SET STATUS = 'started', startTime = '${dateStamp}' WHERE id = '${runObj.id}'`);
+    } else if (runObj.status === 'started') {
+      return db.query(`UPDATE RUNS SET STATUS = 'finished', finishTime = '${dateStamp}' WHERE id = '${runObj.id}'`);
     }
   }
 
   updateRun(runObj) {
-    return db.query(`UPDATE RUNS SET userId = ${runObj.userId}, location = '${runObj.location}', status = '${runObj.status}', title = '${runObj.title}', description = '${runObj.description}' WHERE title = '${runObj.title}'`);
+    return db.query(`UPDATE RUNS SET userId = ${runObj.userId}, runnerId = ${runObj.userId}, amount = '${runObj.userId}', location = '${runObj.location}', status = '${runObj.status}', title = '${runObj.title}', description = '${runObj.description}', expectedFinishTime = '${runObj.expectedFinishTime}', startTime = '${runObj.startTime}', finishTime = '${runObj.finishTime}', WHERE title = '${runObj.title}'`);
   }
 
 }
